c07dfbf553a60efb86bbfd5cf7f210de8196daf6
Adam Schafer <PseudoDesign@users.noreply.github.com>
2019-04-10 00:38:49 -0600

    added custom bitbake

--- rakefile.rb
+++ rakefile.rb
@@ -1,13 +1,13 @@
 namespace :dev do
   RUN_SCRIPT = File.join(__dir__, 'scripts', 'oe-run')
-  def bitbake(project, image)
-    cmd = "#{RUN_SCRIPT} #{project} \"bitbake #{image}\""
+  def bitbake(project, target)
+    cmd = "#{RUN_SCRIPT} #{project} \"bitbake #{target}\""
     sh cmd
   end
 
   desc 'Build a local development image'
-  task :build, [:environment, :image] do |_task, args|
-    bitbake(args[:environment], args[:image])
+  task :build, [:environment, :target] do |_task, args|
+    bitbake(args[:environment], args[:target])
   end
 end
 
@@ -18,6 +18,11 @@ namespace :celestial do
     desc "Build the #{IMAGE_NAME} image in #{BUILD_NAME}"
     task :build do
       Rake::Task['dev:build'].invoke(IMAGE_NAME, BUILD_NAME)
+    end
+
+    desc "Run the provided bitbake command string in the #{BUILD_NAME} context"
+    task :bitbake, [:target] do |_task, args|
+      Rake::Task['dev:build'].invoke(IMAGE_NAME, args[:target])
     end
   end
 end
